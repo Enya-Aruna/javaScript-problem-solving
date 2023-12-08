a26f978e695e7354e30e729965e6412acb4f8b7b
salma islam <salmaislam489265@gmail.com>
2023-12-08 08:56:52 +0600

    js solveing problem

---
+++ duplicate.js
@@ -0,0 +1,16 @@
+const nameList = ['salam' , 'tamanna' , 'saima' , 'sadia' , 'samira' , 'meharu' , 'jiniya' , 'salam' , 'saima'];
+
+function removeDuplicate(names){
+    const unique= [] ;
+    for(let i = 0  ; i < nameList.length ; i++){
+        const name = nameList[i];
+        if(unique.includes(name) === false){
+            unique.push(name)
+        }
+
+    }
+    return unique;
+}
+
+const uniqueName = removeDuplicate(nameList);
+console.log(uniqueName)
